60fac86cc9c8ac7e5418e5173857cad4dcc387ad
Yngve <yngve@ranestad.no>
2014-10-20 20:15:28 +0200

    add RSS i doc

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -11,7 +11,39 @@ document.addEventListener('DOMContentLoaded', function() {
 $(function() {
 
   console.log('Fronter Feeder starting...');
+  //var last = document.getElementById('last');
+  //var passord = document.getElementById('passord');
+  //var brukernavn = document.getElementById('brukernavn');
+ var url = 'https://fronter.com/uia/rss/get_today_rss.php?elements=default&LANG=no';
 
-  
+$('#last').click(function(){
+
+  console.log('Logger på med brukernavn: ', $('#brukernavn').val());
+
+
+  var jqxhr = $.ajax({url: url, username: $('#brukernavn').val(), password: $('#passord').val()} )
+    .done(function(data) {
+
+      var $xml = $(data);
+      $xml.find("item").each(function() {
+
+        var $this = $(this);
+
+        console.log($this.find("title").text());
+
+      });
+      
+      console.log(data);
+      console.log( "success" );
+
+    })
+    .fail(function() {
+      console.log( "error" );
+    })
+    .always(function() {
+      console.log( "complete" );
+    });
+});
+
 
 });
